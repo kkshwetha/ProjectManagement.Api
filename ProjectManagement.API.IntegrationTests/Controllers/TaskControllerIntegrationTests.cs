cdd0c6b31cbaa56f0cf881dc534710ae51446d96
kkshwetha <79935578+kkshwetha@users.noreply.github.com>
2021-04-08 18:14:18 +0530

    User integration tests added

--- ProjectManagement.API.IntegrationTests/Controllers/TaskControllerIntegrationTests.cs
+++ ProjectManagement.API.IntegrationTests/Controllers/TaskControllerIntegrationTests.cs
@@ -45,11 +45,21 @@ namespace ProjectManagement.API.IntegrationTests.Controllers
             // Must be successful.
             httpResponse.EnsureSuccessStatusCode();
 
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+        [Fact]
+        public async System.Threading.Tasks.Task CanGetTasks_TestValues()
+        {
+            // The endpoint or route of the controller action.
+            var httpResponse = await _client.GetAsync("/api/Task");
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
             // Deserialize and examine results.
             var stringResponse = await httpResponse.Content.ReadAsStringAsync();
-            var project = JsonConvert.DeserializeObject<IEnumerable<Task>>(stringResponse);
-            Assert.Contains(project, p => p.Project.Name == "TestProject1");
-            Assert.Contains(project, p => p.Detail == "Create Database Table");
+            var task = JsonConvert.DeserializeObject<IEnumerable<Task>>(stringResponse);
+            Assert.Contains(task, p => p.Detail == "Create Database Table");
         }
 
         [Fact]
